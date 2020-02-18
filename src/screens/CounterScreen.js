87b944b3315c99eef087546bbadf708fcc854dfd
aaron-wc <likeclem30@gmail.com>
2020-02-18 17:39:51 +0100

    implement reducers for counter demo

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,30 +1,38 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import { View, Text, StyleSheet, Button} from 'react-native';
 
+const reducer = (state, action) => {
+    //state === { count: number}
+    //action === {type: 'increment' || 'decrement', payload:1}
+    switch (action.type){
+        case 'increment':
+            return {...state, count: state.count + action.payload }
+        case 'decrement':
+            return {...state, count: state.count - action.payload }
+        default:
+            return state;
+    }
+};
 const CounterScreen = () => {
-    //let counter = 0;
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { count: 0})
+    const {count} = state
+    
 
     return(
         <View>
             <Button
             title="Increase" 
             onPress={() => {
-                //We dont modify state directly
-                //counter++; 
-                setCounter(counter + 1)
-                console.log(counter);
+                dispatch({ type: 'increment', payload: 1 })
                 }} 
             />
             <Button
             title="Decrease" 
             onPress={() => {
-                //counter--; 
-                //console.log(counter); 
-                setCounter(counter - 1)
+                dispatch({ type: 'decrement', payload: 1 })
                 }} 
                 />
-            <Text>Current Counter :{counter}</Text>
+            <Text>Current Counter :{count}</Text>
         </View>
     );
 };
